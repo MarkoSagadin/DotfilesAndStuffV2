b762fd92a0eb36893b31d598db67b32449f3e935
MarkoSagadin <marko.sagadin42@gmail.com>
2023-04-01 17:11:34 +0200

    Simplify gitsigns config

--- dotfiles/.config/nvim/lua/user/plugins/gitsigns.lua
+++ dotfiles/.config/nvim/lua/user/plugins/gitsigns.lua
@@ -1,15 +1,11 @@
 require("gitsigns").setup({
 	signs = {
-		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = {
-			hl = "GitSignsChange",
-			text = "▎",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
+		add = { text = "▎" },
+		change = { text = "▎" },
+		delete = { text = "契" },
+		topdelete = { text = "契" },
+		changedelete = { text = "~" },
+		untracked = { text = "┆" },
 	},
 	preview_config = {
 		-- Options passed to nvim_open_win
