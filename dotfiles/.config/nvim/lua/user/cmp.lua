be138ccf85d424d6b390ec08b902e7a51f312cde
MarkoSagadin <marko.sagadin42@gmail.com>
2023-09-27 08:54:56 +0200

    Remove copilot from cmp

--- dotfiles/.config/nvim/lua/user/cmp.lua
+++ dotfiles/.config/nvim/lua/user/cmp.lua
@@ -104,7 +104,6 @@ cmp.setup({
 				buffer = "[Buffer]",
 				nvim_lua = "[Lua]",
 				path = "[Path]",
-				copilot = "[Copilot]",
 			})[entry.source.name]
 
 			return vim_item
@@ -124,7 +123,6 @@ cmp.setup({
 		{ name = "nvim_lua", group_index = 1 },
 		{ name = "path", group_index = 1 },
 		{ name = "buffer", group_index = 2 },
-		{ name = "copilot", group_index = 1 },
 	},
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
