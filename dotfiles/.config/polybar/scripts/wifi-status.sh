1efe5b4186db0adfefe1d991dad0f9f16fe808cc
MarkoSagadin <marko.sagadin42@gmail.com>
2023-10-29 10:30:14 +0100

    Fix missing wifi icon in the polybar
    After boot the wifi icon might be missing. The issues was that the
    response of "nmcli -fields CONNECTIVITY g" could return "limited"
    output, which was not handled in the if statement, so that final STATUS
    variable was empty.
    
    I also cleaned up redundant statements.

--- dotfiles/.config/polybar/scripts/wifi-status.sh
+++ dotfiles/.config/polybar/scripts/wifi-status.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-SIGNAL_STRENGHT=$(
-	nmcli dev wifi list | awk '/\*/{if (NR!=1) {print $9}}' |
-		sed "s/▂▄▆█/󰤨/g" |
-		sed "s/▂▄▆_/󰤥/g" |
-		sed "s/▂▄__/󰤢/g" |
-		sed "s/▂___/󰤟/g"
-)
-
-STATE=$(nmcli g | awk '/\*/{if (NR!=1) {print $0}}')
-
 # get current connection status
 CONSTATE=$(nmcli -fields CONNECTIVITY g)
-if [[ "$CONSTATE" =~ "none" ]]; then
+if [[ "$CONSTATE" =~ "none" || "$CONSTATE" =~ "limited" ]]; then
 	WIFISTATE=$(nmcli -fields WIFI g)
 	if [[ "$WIFISTATE" =~ "disabled" ]]; then
 		STATUS="󰤮"
