9e1f29b68f841228ef4eba4a7144ffd3d4fc4c4e
MarkoSagadin <marko.sagadin42@gmail.com>
2023-12-04 08:07:02 +0100

    Update picom deps

--- installation_scripts/4_i3_setup.sh
+++ installation_scripts/4_i3_setup.sh
@@ -72,7 +72,7 @@ else
 		libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev \
 		libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev \
 		libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev \
-		libevdev-dev uthash-dev libev-dev libx11-xcb-dev
+		libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-dpms0-dev
 
 	git clone https://github.com/yshui/picom
 	cd picom
