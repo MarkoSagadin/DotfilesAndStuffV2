afbe67f2dd84623b4008f89bef2ee31da2e24e11
MarkoSagadin <marko.sagadin42@gmail.com>
2024-06-26 18:09:59 +0200

    Add instructions for setting up audio

--- installation_scripts/5_manual_instructions.sh
+++ installation_scripts/5_manual_instructions.sh
@@ -3,5 +3,15 @@ Open /usr/share/X11/xkb/symbols/us
 and switch y and z
 You can reload with setxkbmap -layout us, but you should really reset the machine.
 
+# To get audio working
+Open /etc/pulse/default.pa
+Add load-module module-alsa-sink device=hdmi:0
+Restart
+Set to use NVIDIA GPU
+optimus-manager --switch nvidia
+Go into audio settings and select HDMI output
+Open Advanced settings and select everything
+Port should be headphones
+
 # Below works on omen
 # GRUB_CMDLINE_LINUX_DEFAULT="quiet nvidia-drm.modeset=1 acpi_osi='Windows 2020' acpi_backlight=vendor udev.log_priority=3 loglevel=3 amd_iommu=off"
