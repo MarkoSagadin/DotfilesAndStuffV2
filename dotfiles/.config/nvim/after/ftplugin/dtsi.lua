be2dfbf68d6118cc452e5fc4475900db3170eef6
MarkoSagadin <marko.sagadin42@gmail.com>
2024-09-18 08:21:02 +0200

    add ftplugin files for devicetree files

---
+++ dotfiles/.config/nvim/after/ftplugin/dtsi.lua
@@ -0,0 +1,19 @@
+---------------------------------
+-- General options for dtsi files
+---------------------------------
+
+-- Construct a table, where keys are on the left, values are on the right
+local options = {
+	tabstop = 4, -- Number of visual spaces per TAB
+	softtabstop = 4, -- Number of spaces in tab when editing
+	shiftwidth = 4, -- The number of spaces inserted for each indentation
+	expandtab = false, -- Convert tabs to spaces
+	colorcolumn = "80", -- Highlight specified column for line wrapin
+	textwidth = 80,
+}
+
+-- Pairs returns key-value pair from table `options` in no particular order
+-- and fills up vim.opt table with them.
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
