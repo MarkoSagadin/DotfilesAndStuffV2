254ca3228e08c3d6fce10142181749e5e8cfedaa
MarkoSagadin <marko.sagadin42@gmail.com>
2024-11-10 12:31:50 +0100

    Perfrom basic linting why pyright, instead of just using Ruff

--- dotfiles/.config/nvim/lua/lsp/settings/pyright.lua
+++ dotfiles/.config/nvim/lua/lsp/settings/pyright.lua
@@ -6,8 +6,7 @@ return {
 		},
 		python = {
 			analysis = {
-				-- Ignore all files for analysis to exclusively use Ruff for linting
-				ignore = { "*" },
+				typeCheckingMode = "basic",
 			},
 		},
 	},
