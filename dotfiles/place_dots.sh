d238693c8c5da5bd08c19e88f7951c3b63d79e35
MarkoSagadin <marko.sagadin42@gmail.com>
2024-11-19 20:29:52 +0200

    Add hyprland config, work in progress

--- dotfiles/place_dots.sh
+++ dotfiles/place_dots.sh
@@ -3,27 +3,27 @@
 dotfilesDir=$(pwd)
 
 function linkDotfile {
-	dest="${1}/${2}"
-	dateStr=$(date +%Y-%m-%d-%H%M)
+    dest="${1}/${2}"
+    dateStr=$(date +%Y-%m-%d-%H%M)
 
-	if [ -h ~/${2} ]; then
-		# Existing symlink
-		echo "Removing existing symlink: ${dest}"
-		sudo rm ${dest}
+    if [ -h ~/${2} ]; then
+        # Existing symlink
+        echo "Removing existing symlink: ${dest}"
+        sudo rm ${dest}
 
-	elif [ -f "${dest}" ]; then
-		# Existing file
-		echo "Backing up existing file: ${dest}"
-		sudo mv ${dest}{,.${dateStr}}
+    elif [ -f "${dest}" ]; then
+        # Existing file
+        echo "Backing up existing file: ${dest}"
+        sudo mv ${dest}{,.${dateStr}}
 
-	elif [ -d "${dest}" ]; then
-		# Existing dir
-		echo "Backing up existing dir: ${dest}"
-		sudo mv ${dest}{,.${dateStr}}
-	fi
+    elif [ -d "${dest}" ]; then
+        # Existing dir
+        echo "Backing up existing dir: ${dest}"
+        sudo mv ${dest}{,.${dateStr}}
+    fi
 
-	echo "Creating new symlink: ${dest}"
-	sudo ln -s ${dotfilesDir}/${2} ${dest}
+    echo "Creating new symlink: ${dest}"
+    sudo ln -s ${dotfilesDir}/${2} ${dest}
 }
 
 # Create config folder if it does not exists, below symlinks fail otherwise
@@ -46,6 +46,7 @@ linkDotfile /home/$USER .config/fontconfig
 linkDotfile /home/$USER .config/icons
 linkDotfile /home/$USER .config/scripts
 linkDotfile /home/$USER .config/zellij
+linkDotfile /home/$USER .config/hyprland
 linkDotfile /home/$USER .ssh
 linkDotfile /home/$USER .gitconfig
 linkDotfile /home/$USER .tmux.conf
