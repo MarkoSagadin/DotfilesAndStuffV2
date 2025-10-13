8efbcb4ca0c5c9b04cc08ae937ac39a613a2cf3e
Marko Sagadin <marko.sagadin42@gmail.com>
2025-10-13 22:51:55 +0200

    fix: make snippets work again

--- dotfiles/.config/nvim/lua/plugins/cmp.lua
+++ dotfiles/.config/nvim/lua/plugins/cmp.lua
@@ -1,7 +1,22 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
-	opts = {},
-	build = "cargo build --release",
-	opts_extend = { "sources.default" },
+	{
+		"saghen/blink.cmp",
+		event = "InsertEnter",
+		dependencies = { "rafamadriz/friendly-snippets", { "L3MON4D3/LuaSnip", version = "v2.*" } },
+		opts = {
+			sources = {
+				default = { "snippets", "lsp", "buffer", "path" },
+			},
+			snippets = { preset = "luasnip" },
+		},
+		build = "cargo build --release",
+		opts_extend = { "sources.default" },
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+		opts = {},
+	},
 }
