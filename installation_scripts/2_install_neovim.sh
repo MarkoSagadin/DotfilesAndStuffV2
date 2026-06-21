a8354ca1243fc9b64ea08625a55dadfa694b67fd
MarkoSagadin <marko.sagadin42@gmail.com>
2026-06-21 18:19:31 +0200

    feat: change tree-sitter-cli install, update neovim version

--- installation_scripts/2_install_neovim.sh
+++ installation_scripts/2_install_neovim.sh
@@ -10,14 +10,14 @@ check_distro
 # Neovim tooling - linters, formatters
 pip install pynvim --break-system-packages
 pip install neovim --break-system-packages
-sudo npm install -g tree-sitter-cli
+cargo install tree-sitter-cli
 
 if [ "$DISTRO" = "MANJARO" ]; then
     # Manjaro specific
     install neovim
 else
     # Ubuntu specific
-    NVIM_TAG="v0.10.2"
+    NVIM_TAG="v0.12.3"
     curl -LO https://github.com/neovim/neovim/releases/download/$NVIM_TAG/nvim-linux64.tar.gz
     sudo rm -rf /opt/nvim
     sudo tar -C /opt -xzf nvim-linux64.tar.gz
